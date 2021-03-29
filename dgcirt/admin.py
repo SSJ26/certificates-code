85f6b3a4b4e7147dbc9fcb3fec4e7628268f3680
SUMIT SINGH JADAUN <38113204+SSJ26@users.noreply.github.com>
2021-03-30 00:25:57 +0530

    designing part

--- dgcirt/admin.py
+++ dgcirt/admin.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 from .models import *
 
 class Person_list(admin.ModelAdmin):
-    list_display=['firstName','secondName','course','courseDetails','companyLink','date','companyName','ceoName']
+    list_display=['firstName','secondName','course','courseDetails','companyLink','companyName','ceoName']
 admin.site.register(Person,Person_list)
--- dgcirt/views.py
+++ dgcirt/views.py
@@ -15,59 +15,87 @@ def register(request):
     if request.method == 'POST':
 
         form = request.POST
-        print(form)
+        issuedDate=request.POST['issuedate']
+        print(issuedDate)
+        # print(form)
         # if form.is_valid():
             # form = form.save()
         print(form['firstName'],form['secondName'],form['course'],form['course']+ '_' + form['firstName'] + form['secondName'] + '.pdf')
-        generate_certificate(form['firstName'],form['secondName'],form['course'],form['course']+ '_' + form['firstName'] + form['secondName'] + '.pdf')
+        generate_certificate(form['firstName'],form['secondName'],form['course'],form['courseDetails'],form['companyLink'],issuedDate,form['companyName'],form['ceoName'],form['course']+ '_' + form['firstName'] + form['secondName'] + '.pdf')
 
-        return render(request, 'download.html', {'data': form,'pdf':form['course']+ '_' + form['firstName'] + form['secondName'] + '.pdf'})
+        return render(request, 'download.html', {'data': form,'issuedDate':issuedDate,'pdf':form['course']+ '_' + form['firstName'] + form['secondName'] + '.pdf'})
         # else:
         #     return HttpResponse('Your Form is wrong')
 
     return render(request, 'register.html', {'forms': PersonForm()})
 
 
-
-# def import_data(data_file):
-#     attendee_data=csv.reader(open(data_file,"rb"))
-#     for row in attendee_data:
-#         last_name=row[0]
-#         first_name=row[1]
-#         course_name=row[2]
-#         pdf_file_name=course_name+ ' ' + last_name + first_name + '.pdf'
-#         generate_certificate(first_name, last_name, course_name, pdf_file_name)
-
-def generate_certificate(first_name, last_name, course_name, pdf_file_name):
+def generate_certificate(first_name, last_name, course_name,course_details, company_link, date_field, company_name, ceo_name, pdf_file_name):
     attendee_name= first_name + ' '+ last_name
     c=canvas.Canvas('dgcirt/static/'+pdf_file_name, pagesize=landscape(letter))
 
     #header text
     c.setFont('Helvetica',48, leading=None)
-    c.drawCentredString(415, 500, "Certification of Completion")
+    c.drawCentredString(400, 550, "Certification of Completion")
     c.setFont('Helvetica',24, leading=None)
-    c.drawCentredString(415, 450, "This certificate is presented to")
+    c.drawCentredString(400, 500, "This certificate is presented to")
 
     # attendee name
     c.setFont('Helvetica-Bold',34, leading=None)
-    c.drawCentredString(415, 395, attendee_name)
+    c.drawCentredString(400, 450, attendee_name)
 
     # for completing the course
     c.setFont('Helvetica',24, leading=None)
-    c.drawCentredString(415, 350, 'for completion the following course:')
+    c.drawCentredString(400, 400, 'for completion the following course:')
     
     #course name
-    c.setFont('Helvetica',20, leading=None)
-    c.drawCentredString(415, 310, course_name)
+    c.setFont('Helvetica-Bold',24, leading=None)
+    c.drawCentredString(400, 360, course_name)
+    
+    #course Details
+    c.setFont('Helvetica',12, leading=None)
+    c.drawCentredString(400, 330, course_details)
+    
+    #Company Link
+    c.setFont('Helvetica',24, leading=None)
+    c.drawCentredString(170, 270, 'Company Link')
+
+    #Link
+    c.setFont('Helvetica',12, leading=None)
+    c.drawCentredString(170, 240, company_link)
+    
+    #Issued Date
+    c.setFont('Helvetica',24, leading=None)
+    c.drawCentredString(620, 270, 'Issued Date')
+
+    #Issued Date
+    c.setFont('Helvetica',12, leading=None)
+    c.drawCentredString(620, 240, date_field)
+
+    #Issued BY
+    c.setFont('Helvetica',24, leading=None)
+    c.drawCentredString(170, 115, 'Issued By:')
+    
+    #Company Name
+    c.setFont('Helvetica',12, leading=None)
+    c.drawCentredString(170, 85,company_name)
 
     # image of seal
     seal='seal.png'
-    c.drawImage(seal, 350, 30, width=None, height=None)
+    c.drawImage(seal, 340, 120, width=None, height=None)
+
+    #Name of ceo
+    c.setFont('Helvetica',24, leading=None)
+    c.drawCentredString(620, 115, 'Name Of CEO')
+
+    #Ceo Name
+    c.setFont('Helvetica',12, leading=None)
+    c.drawCentredString(620, 85, ceo_name)
+    
 
     c.showPage()
     print('writing')
     c.save()
 
-# import_data(data_file)
 
 
